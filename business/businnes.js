ac3a55aa7e32d96361f8a0abadbf9b6c94825047
FrirexD <frirex.go@gmail.com>
2023-03-20 18:57:42 +0100

    API FONCTION + BUISNeSS STARTED

---
+++ business/businnes.js
@@ -0,0 +1,29 @@
+//importation des modules
+
+
+const express = require("express");
+const buisness = require("../data/data")
+
+// Initisation de l'application 
+
+
+// Fonctions à exporter 
+
+const buisness_public {
+    get_all_users : ( ) => {
+        res.
+    },
+    add_users : () => {
+        
+    },
+        edit_users : ( ) => {
+        
+    },
+    delete_users : ( ) => {
+        
+    }
+};
+
+
+// Exportation des module
+module.exports = buisness_public; 
--- presentation/api.js
+++ presentation/api.js
@@ -18,10 +18,48 @@ const api = {
         app.use(express.json());
 
         //Toutes les requetes
-        app.get(RESQUEST_URL, buisness.get_all_users);
-        app.post(RESQUEST_URL, buisness.add_user);
-        app.put(RESQUEST_URL, buisness.edit_user);
-        app.delete(RESQUEST_URL, buisness.delete_user);
+
+        //requete de GET qui renvoie un json qui contient tous les utilisateurs
+        app.get(RESQUEST_URL, (req , res ,next) => {
+            res.json(buisness.get_all_users());
+        });
+
+        //requete de post ajoute un utilisateur dans la BDD
+        app.post(RESQUEST_URL, (req , res ,next) => {
+            let is_added= buisness.add_user(req.body);
+
+            //renvoie les codes de réussite ou echec
+            if(is_added){
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(400);
+            }
+        });
+        
+        //requete de Edit user qui modifie l'utilisateur dans la BDD
+        app.put(RESQUEST_URL, (req , res ,next) => {
+            let is_edit= buisness.edit_user(req.body);
+
+            //renvoie les codes de réussite ou echec
+            if(is_edit){
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(400);
+            }
+        });
+        
+        //
+        app.delete(RESQUEST_URL, (req , res ,next) => {
+            let is_delete= buisness.delete_user(req.body);
+
+            //renvoie les codes de réussite ou echec
+            if(is_delete){
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(400);
+            }
+        });
+        
 
         //starts listening the port 
         console.log(`App listening to port ${port} `)
