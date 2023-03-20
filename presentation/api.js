9d634f42b6f2077c04a8c791f6bf1751b4d5f4d8
FrirexD <frirex.go@gmail.com>
2023-03-20 18:33:15 +0100

    add fonction to api

--- presentation/api.js
+++ presentation/api.js
@@ -3,3 +3,31 @@
 
 const express = require("express");
 const buisness = require("../business/business.js")
+const RESQUEST_URL = "/users";
+
+// Initisation de l'application 
+
+let app = express();
+
+// API backend 
+const api = {
+    
+    //start l'API sur le port 
+    start : port => { app.listen(port, () => {
+        // autorise l'utilisation des Json
+        app.use(express.json());
+
+        //Toutes les requetes
+        app.get(RESQUEST_URL, buisness.get_all_users);
+        app.post(RESQUEST_URL, buisness.add_user);
+        app.put(RESQUEST_URL, buisness.edit_user);
+        app.delete(RESQUEST_URL, buisness.delete_user);
+
+        //starts listening the port 
+        console.log(`App listening to port ${port} `)
+    })}
+};
+
+// Exportation des module
+
+module.exports = api; 
